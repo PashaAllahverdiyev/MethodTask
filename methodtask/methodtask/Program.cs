8f719dda89f2547bf3f264ca867ec36e3892f6f9
PashaAllahverdiyev <129064604+PashaAllahverdiyev@users.noreply.github.com>
2024-03-18 10:33:12 +0400

    Add files via upload

---
+++ methodtask/methodtask/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Drawing;
+//{
+//    Sade();
+//}
+//static void Sade()
+//{
+//    int num =Convert.ToInt32(Console.ReadLine());
+//    if (num % 2 == 1)
+//    {
+//        Console.WriteLine("sadedir");
+//    }
+//    else
+//    {
+//        Console.WriteLine("deyil");
+//    }
+//}
+
+
+{
+    Point();
+}
+static void Point()
+{
+    Console.WriteLine("Balinizi daxil edin: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    int b = Convert.ToInt32(Console.ReadLine());
+    if (a + b > 60)
+    {
+        Console.WriteLine("Mezun oldunuz tebrikler");
+    }
+    else {
+        Console.WriteLine("ola bilmediz");
+            }
+}
